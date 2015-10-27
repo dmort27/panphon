55cee33a198d930161790b95647ec4879923d9de
David R. Mortensen <dmortens@cs.cmu.edu>
2015-10-27 15:52:57 -0400

    Added comments.

--- panphon/panphon.py
+++ panphon/panphon.py
@@ -4,11 +4,20 @@ import unicodecsv as csv
 
 
 class FeatureTable(object):
+    """Encapsulate the segment <=> feature mapping in the file
+    data/segment_features.csv.
+
+    """
     def __init__(self):
-        self._read_table(self)
-        self.seg_dict = dict(self.segments)
+        self._read_table()
 
     def _read_table(self):
+        """Read the data from data/segment_features.csv into self.segments, a
+        list of 2-tuples of unicode strings and sets of feature tuples
+        and self.seg_dict, a dictionary mapping from unicode segments
+        and sets of feature tuples.
+
+        """
         self.segments = []
         with open('data/segment_features.csv', 'rb') as f:
             reader = csv.reader(f, encoding='utf-8')
@@ -19,8 +28,15 @@ class FeatureTable(object):
                 vals = row[1:]
                 specs = set(zip(vals, names))
                 self.segments.append((seg, specs))
+        self.seg_dict = dict(self.segments)
+        # A few sanity checks:
+        assert (u'+', u'cons') in self.seg_dict[u'tʰ']
+        assert set[(u'-', u'cons'), (u'-', u'hi')] < self.seg_dict[u'a']
 
     def feature_match(self, features, segment):
+        """Evaluates whether a set of features 'match' a segment (are a subset
+        of that segment's features).
+        """
         return segment in [s for (s, fts)
                            in self.segments
                            if features <= fts]
