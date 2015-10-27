7d9fc586e4a9ccfcea963d0a8731945b0e8d5df1
David R. Mortensen <dmortens@cs.cmu.edu>
2015-10-27 09:23:20 -0400

    Added basic functionality

--- panphon/panphon.py
+++ panphon/panphon.py
@@ -1,5 +1,26 @@
 # -*- coding: utf-8 -*-
 
+import unicodecsv as csv
+
 
 class FeatureTable(object):
-    pass
+    def __init__(self):
+        self._read_table(self)
+        self.seg_dict = dict(self.segments)
+
+    def _read_table(self):
+        self.segments = []
+        with open('data/segment_features.csv', 'rb') as f:
+            reader = csv.reader(f, encoding='utf-8')
+            header = reader.next()
+            names = header[1:]
+            for row in reader:
+                seg = row[0]
+                vals = row[1:]
+                specs = set(zip(vals, names))
+                self.segments.append((seg, specs))
+
+    def feature_match(self, features, segment):
+        return segment in [s for (s, fts)
+                           in self.segments
+                           if features <= fts]
