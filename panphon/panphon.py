3483a5fa46e2bc2b0dc710e91c4b8a893f0ee13c
David R. Mortensen <dmortens@cs.cmu.edu>
2015-10-28 10:45:05 -0400

    Added an debugged important functions

--- panphon/panphon.py
+++ panphon/panphon.py
@@ -1,8 +1,15 @@
 # -*- coding: utf-8 -*-
 
+import sys
 import pkg_resources
 import unicodecsv as csv
 
+
+class FeatureError(Exception):
+    pass
+
+class SegmentError(Exception):
+    pass
 
 class FeatureTable(object):
     """Encapsulate the segment <=> feature mapping in the file
@@ -12,12 +19,12 @@ class FeatureTable(object):
     def __init__(self):
         self._read_table()
         # Sanity checks for self.feature_match
-        # assert self.feature_match(
-        #     set([(u'+', u'sg'), (u'-', u'syl'), (u'-', u'cor')]), u'pʰ')
-        # assert self.feature_match(
-        #     set([(u'-', u'tense'), (u'+', u'hi'), (u'-', u'back')]), u'ɪ')
-        # assert self.feature_match(
-        #     set([(u'+', u'nas'), (u'-', u'voi'), (u'+', u'cor')]), u'n̥')
+        assert self.feature_match(
+            set([(u'+', u'sg'), (u'-', u'syl'), (u'-', u'cor')]), u'pʰ')
+        assert self.feature_match(
+            set([(u'-', u'tense'), (u'+', u'hi'), (u'-', u'back')]), u'ɪ')
+        assert self.feature_match(
+            set([(u'+', u'nas'), (u'-', u'voi'), (u'+', u'cor')]), u'n̥')
 
     def _read_table(self):
         """Read the data from data/segment_features.csv into self.segments, a
@@ -39,11 +46,52 @@ class FeatureTable(object):
                 self.segments.append((seg, specs))
         self.seg_dict = dict(self.segments)
         # A few sanity checks:
-        # assert (u'+', u'cons') in self.seg_dict[u'tʰ']
-        # assert set([(u'-', u'cons'), (u'-', u'hi')]) <= self.seg_dict[u'a']
+        assert (u'+', u'cons') in self.seg_dict[u'tʰ']
+        assert set([(u'-', u'cons'), (u'-', u'hi')]) <= self.seg_dict[u'a']
 
     def feature_match(self, features, segment):
         """Evaluates whether a set of features 'match' a segment (are a subset
-        of that segment's features).
+        of that segment's features); returns 'None' if segment is unknown.
         """
-        return features <= self.seg_dict[segment]
+        features = set(features)
+        if segment in self.seg_dict:
+            return features <= self.seg_dict[segment]
+        else:
+            return None
+
+    def segment_features(self, segment):
+        """Returns the features as a list of 2-tuples, given a segment as a
+        Unicode string; returns 'None' if segment is unknown.
+
+        segment -- segment for which features are to be returned as
+        Unicode string """
+        if segment in self.seg_dict:
+            return self.seg_dict[segment]
+        else:
+            return None
+
+    def feature_intersection(self, segments):
+        """Returns the features shared by all segments in the list/set of
+        segments. Segments that are not known are ignored error.
+
+        segments -- set/list of features
+        """
+        segments = set([seg for seg
+                        in segments
+                        if seg in self.seg_dict])
+        seg1 = segments.pop()
+        fts = self.seg_dict[seg1]
+        for seg in segments:
+            fts = fts & self.seg_dict[seg]
+        return fts
+
+    def features_match_inv(self, fts, inv):
+        """Returns a boolean based on whether there is a segment in 'inv'
+        that matches all of the features in 'features'.
+
+        features -- a collection of feature 2-tuples <val, name>
+        inv -- a collection of segments represented as Unicode
+               strings
+
+        """
+        return any([self.feature_match(fts, s) for s in inv])
