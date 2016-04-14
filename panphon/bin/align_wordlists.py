24a2ea6228a5159e9a946bb18839bab92e70e09f
David R. Mortensen <dmortens@cs.cmu.edu>
2016-04-14 10:01:27 -0400

    A number of small edits

--- panphon/bin/align_wordlists.py
+++ panphon/bin/align_wordlists.py
@@ -48,8 +48,9 @@ def main(wordlist1, wordlist2, dist):
         reader_a = csv.reader(file_a, encoding='utf-8')
         reader_b = csv.reader(file_b, encoding='utf-8')
         print('Reading word lists...')
-        words_a = [(w, g) for (g, w) in reader_a]
-        words_b = [(w, g) for (g, w) in reader_b]
+        words = zip([(w, g) for (g, w) in reader_a],
+                    [(w, g) for (g, w) in reader_b])
+        words_a, words_b = zip(*[(a, b) for (a, b) in words if a and b])
         print('Constructing cost matrix...')
         matrix = construct_cost_matrix(words_a, words_b, dist)
         m = munkres.Munkres()
