9f953f583a579511d12f72cb3775b4fee8e2f6db
David R. Mortensen <davidmortensen@gmail.com>
2016-05-26 16:52:05 -0400

    Fine-tuned tests

--- panphon/test_distance.py
+++ panphon/test_distance.py
@@ -28,12 +28,30 @@ class TestDogolPrime(unittest.TestCase):
         self.assertEqual(self.dist.dogol_prime_distance('pop', 'bab'), 0)
 
 
-class TestUnweightedFtEditDist(unittest.TestCase):
+class TestUnweightedFeatureEditDist(unittest.TestCase):
+    def setUp(self):
+        self.dist = distance.Distance()
+
+    def test_unweighted_substitution_cost(self):
+        self.assertEqual(self.dist.unweighted_substitution_cost(['0', '+', '-'], ['0', '+', '+']), 1)
+
+    def test_unweighted_deletion_cost(self):
+        self.assertEqual(self.dist.unweighted_deletion_cost(['+', '-', '+', '0']), 3.5)
+
+    def test_trivial1(self):
+        self.assertEqual(self.dist.feature_edit_distance('bim', 'pym'), 3)
+
+    def test_trivial2(self):
+        self.assertEqual(self.dist.feature_edit_distance('ti', 'tʰi'), 1)
+
+class TestWeightedFeatureEditDist(unittest.TestCase):
     def setUp(self):
         self.dist = distance.Distance()
 
     def test_trivial1(self):
-        self.assertEqual(self.dist.feature_edit_distance(self.dist.segs('pim'), self.dist.segs('bym')), 2)
+        self.assertGreater(self.dist.weighted_feature_edit_distance('ti', 'tʰu'),
+                           self.dist.weighted_feature_edit_distance('ti', 'tʰi'))
 
-    def test_trivial2(self):
-        self.assertEqual(self.dist.feature_edit_distance(self.dist.segs('ti'), self.dist.segs('tʰi')), 1)
+    def test_trivial1(self):
+        self.assertGreater(self.dist.weighted_feature_edit_distance('ti', 'te'),
+                           self.dist.weighted_feature_edit_distance('ti', 'tḭ'))
