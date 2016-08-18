8db5fc7b8e57a472ff722ef3586975e78e9e9ecf
David R. Mortensen <davidmortensen@gmail.com>
2016-08-18 11:10:33 -0400

    Fixed errors in test_panphon.py

--- panphon/test_panphon.py
+++ panphon/test_panphon.py
@@ -11,7 +11,7 @@ class TestFeatureTable(unittest.TestCase):
         self.ft = _panphon.FeatureTable()
 
     def test_fts_contrast2(self):
-        inv = 'p t k b d g a e i o u'.split(' ')
+        inv = 'p t k b d ɡ a e i o u'.split(' ')
         self.assertTrue(self.ft.fts_contrast2([('-', 'syl')], 'voi', inv))
         self.assertFalse(self.ft.fts_contrast2([('+', 'syl')], 'cor', inv))
         self.assertTrue(self.ft.fts_contrast2(_panphon.fts('+ant -cor'), 'voi', inv))
@@ -32,56 +32,6 @@ class TestIpaRe(unittest.TestCase):
         self.assertIsNotNone(r.match('pj'))
         self.assertIsNone(r.match('ts'))
 
-
-class TestSonority(unittest.TestCase):
-
-    def setUp(self):
-        self.ft = _panphon.FeatureTable()
-
-    def test_sonority_nine(self):
-        segs = ['a', 'ɑ', 'æ', 'ɒ', 'e', 'o']
-        scores = [9] * 6
-        self.assertEqual(map(self.ft.sonority, segs), scores)
-
-    def test_sonority_eight(self):
-        segs = ['i', '', 'ɨ', 'ʉ', 'ɯ', '']
-        scores = [8] * 6
-        self.assertEqual(map(self.ft.sonority, segs), scores)
-
-    def test_sonority_seven(self):
-        segs = ['j', 'w', 'ʋ', 'ɰ', 'ɹ']
-        scores = [7] * 5
-        self.assertEqual(map(self.ft.sonority, segs), scores)
-
-    def test_sonority_six(self):
-        segs = ['l', 'ɭ', 'r', 'ɾ']
-        scores = [6] * 4
-        self.assertEqual(map(self.ft.sonority, segs), scores)
-
-    def test_sonority_five(self):
-        segs = ['n', 'm', 'ŋ', 'ɴ']
-        scores = [5] * 4
-        self.assertEqual(map(self.ft.sonority, segs), scores)
-
-    def test_sonority_four(self):
-        segs = ['v', 'z', 'ʒ', 'ɣ']
-        scores = [4] * 4
-        self.assertEqual(map(self.ft.sonority, segs), scores)
-
-    def test_sonority_three(self):
-        segs = ['f', 's', 'x', 'ħ', 'ʃ']
-        scores = [3] * 5
-        self.assertEqual(map(self.ft.sonority, segs), scores)
-
-    def test_sonority_two(self):
-        segs = ['b', 'g', 'd', 'ɢ']
-        scores = [2] * 4
-        self.assertEqual(map(self.ft.sonority, segs), scores)
-
-    def test_sonority_one(self):
-        segs = ['p', 'k', 'c', 'q']
-        scores = [1] * 4
-        self.assertEqual(map(self.ft.sonority, segs), scores)
 
 if __name__ == '__main__':
     unittest.main()
