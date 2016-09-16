116cb68467ba56ebefdfc861d9258dc4f950f82e
David R. Mortensen <dmortens@cs.cmu.edu>
2016-09-15 21:19:22 -0400

    Added test_panphon_meethods.py, containing basic units tests

---
+++ panphon/test_panphon_methods.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals, print_function
+
+import unittest
+import _panphon
+
+
+class TestFeatureTableAPI(unittest.TestCase):
+
+    def setUp(self):
+        self.ft = _panphon.FeatureTable()
+
+    def test_fts(self):
+        self.assertEqual(len(self.ft.fts('u')), 21)
+
+    def test_match(self):
+        self.assertTrue(self.match(self.ft.fst('u'), self.ft.fts('u')))
+
+    def test_fts_match(self):
+        self.assertTrue(self.fts_match(self.ft.fst('u'), 'u'))
+
+    def test_longest_one_seg_prefix(self):
+        self.assertEqual(self.ft.longest_one_seg_prefix('pap'), 'p')
+
+    def test_validate_word(self):
+        self.assertTrue(self.ft.validate_word('tik'))
+
+    def test_segs(self):
+        self.assertEqual(self.ft.segs('tik'), ['t', 'i', 'k'])
+
+    def test_word_fts(self):
+        self.assertEqual(len(self.ft.word_fts('tik')), 3)
+
+    def test_seg_known(self):
+        self.assertTrue(self.ft.seg_known('t'))
+
+    def test_filter_string(self):
+        self.assertEqual(len(self.ft.filter_string('pup$')), 3)
+
+    def test_seg_fts(self):
+        self.assertEqual(len(self.ft.seg_fts('p')), 21)
+
+    def test_segs_safe(self):
+        self.assertEqual(len(self.ft.segs_safe('pup$')), 4)
+
+    def test_filter_segs(self):
+        self.assertEqual(len(self.ft.filter_segs(['p', 'u', 'p', '$'])), 3)
+
+    def test_fts_intersection(self):
+        self.assertIn(('-', 'voi'), self.ft.fts_intersection(['p', 't', 'k']))
+
+    def test_fts_match_any(self):
+        self.assertTrue(self.ft.fts_match_any([('-', 'voi')], ['p', 'o', '$']))
+
+    def test_fts_match_all(self):
+        self.assertTrue(self.ft.fts_match_all([('-', 'voi')], ['p', 't', 'k']))
+
+    def test_fts_contrast2(self):
+        self.assertTrue(self.ft.fts_contrast2([], 'voi', ['p', 'b', 'r']))
+
+    def test_fts_count(self):
+        self.assertEqual(self.ft.fts_count([('-', 'voi')], ['p', 't', 'k', 'r']), 3)
+
+    def test_match_pattern(self):
+        self.assertEqual(self.ft.match_pattern([set([('-', 'voi')])], 'p'), 'p')
+
+    def test_match_pattern_seq(self):
+        self.assertTrue(self.ft.match_pattern_seq([set([('-', 'voi')])], 'p'))
+
+    def test_all_segs_matching_fts(self):
+        self.assertIn('p', self.ft.all_segs_matching_fts([('-', 'voi')]))
+
+    def test_compile_regex_from_str(self):
+        pass
+
+    def test_segment_to_vector(self):
+        self.assertEqual(len(self.ft.segment_to_vector('p')), 21)
+
+    def test_word_to_vector_list(self):
+        self.assertEqual(len(self.ft.word_to_vector_list('pup')), 4)
