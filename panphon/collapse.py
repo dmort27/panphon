931a85a43d0741ccec99fbdd2d45343321121fa3
David R. Mortensen <davidmortensen@gmail.com>
2017-02-07 14:45:56 -0500

    Added Collapser

---
+++ panphon/collapse.py
@@ -0,0 +1,36 @@
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+
+import os.path
+
+import yaml
+
+from . import _panphon
+
+
+class Collapser(object):
+    def __init__(self, tablename='dogolpolsky_prime.yml', feature_set='spe+', feature_model='strict'):
+        fm = {'strict': _panphon.FeatureTable,
+              'permissive': permissive.PermissiveFeatureTable}
+        self.fm = fm[feature_model](feature_set=feature_set)
+        self.rules = self._load_table(table)
+
+    def _load_table(self, tablename):
+        fn = os.path.join('data', tablename)
+        fn = pkg_resources.resource_filename(__name__, fn)
+        with open(filename, 'r') as f:
+            rules = []
+            table = yaml.load(f.read())
+            for rule in table:
+                rules.append(_panphon.fts(rule['def']), rule['label'])
+        return rules
+
+    def collapse(self, s):
+        segs = []
+        for seg in self.fm.seg_regex.findall(s):
+            fts = self.fs.fts(seg)
+            for mask, label in self.rules:
+                if self.fm.match(mask, fts):
+                    segs.append(label)
+                    break
+        return ''.join(segs)
