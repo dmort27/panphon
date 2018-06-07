ab38b251d9c73d98db3370ff13b04c587e88765c
David R. Mortensen <davidmortensen@gmail.com>
2018-06-07 16:48:16 -0400

    Migrated generate_ipa_all.py to Python 3

--- panphon/bin/generate_ipa_all.py
+++ panphon/bin/generate_ipa_all.py
@@ -4,40 +4,66 @@ from __future__ import print_function, unicode_literals
 import argparse
 import codecs
 import copy
-import os.path
-from types import DictType, ListType, StringTypes
+# from types import DictType, ListType, StringTypes
 
-import pkg_resources
 import yaml
 
 import unicodecsv as csv
 
 
 class Segment(object):
+    """Class modeling phonological segment."""
+
     def __init__(self, form, features):
-        assert isinstance(form, StringTypes)
+        """Construct Segment objectself.
+
+        Args:
+            form (string): the segment as ipa
+            features (list): the segment as feature_names
+        """
         self.form = form
-        assert isinstance(features, DictType)
         self.features = features
 
     def __repr__(self):
-        return 'Segment("{}", {})'.format(self.form, repr(self.features)).encode('utf-8')
+        """Output string representation of Segment."""
+        return 'Segment("{}", {})'.format(self.form,
+                                          repr(self.features)).encode('utf-8')
 
     def feature_vector(self, feature_names):
-        assert set(feature_names) == set(self.features.keys())
+        """Return feature vector for segment.
+
+        Args:
+            feature_names (list): ordered names of features
+
+        Returns:
+            list: feature values
+        """
         return [self.features[ft] for ft in feature_names]
 
 
 class Diacritic(object):
+    """An object encapsulating a diacritics properties."""
+
     def __init__(self, marker, position, conditions, exclude, content):
+        """Construct a diacritic object.
+
+        Args:
+            marker (unicode): the string form of the diacritic
+            position (str): 'pre' or 'post', determining whether the diacritic
+                            attaches before or after the base
+            conditions (list): feature specification on which application of
+                               diacritic is conditional
+            exclude (list): conditions under which the diacritic will not be
+                            applied]
+            content (list): feature specification that will override
+                            existing feature specifications when diacritics
+                            is applied
+        """
         self.marker = marker
         assert position in ['pre', 'post']
         self.position = position
-        assert isinstance(exclude, ListType)
         self.exclude = exclude
-        assert isinstance(conditions, ListType)
         self.conditions = conditions
-        assert isinstance(content, DictType)
         self.content = content
 
     def match(self, segment):
@@ -65,7 +91,6 @@ class Diacritic(object):
 
 class Combination(object):
     def __init__(self, diacritics, name, sequence):
-        assert isinstance(diacritics, DictType)
         self.name = name
         self.sequence = [diacritics[d] for d in sequence]
 
@@ -103,7 +128,8 @@ def parse_dia_defs(dia_defs):
                                             dia['content'])
     combinations = []
     for comb in defs['combinations']:
-        combinations.append(Combination(diacritics, comb['name'], comb['combines']))
+        combinations.append(Combination(diacritics, comb['name'],
+                                        comb['combines']))
     return diacritics, combinations
 
 
@@ -111,7 +137,8 @@ def sort_all_segments(sort_order, all_segments):
     all_segments_list = list(all_segments)
     field_order = reversed(yaml.load(open(sort_order, 'r').read()))
     for field in field_order:
-        all_segments_list.sort(key=lambda seg: seg.features[field['name']], reverse=field['reverse'])
+        all_segments_list.sort(key=lambda seg: seg.features[field['name']],
+                               reverse=field['reverse'])
     return all_segments_list
 
 
@@ -131,24 +158,20 @@ def write_ipa_all(ipa_bases, ipa_all, all_segments, sort_order):
 
 def main(ipa_bases, ipa_all, dia_defs, sort_order):
     segments = read_ipa_bases(ipa_bases)
-    assert isinstance(segments, ListType)
     diacritics, combinations = parse_dia_defs(dia_defs)
-    assert isinstance(diacritics, DictType)
-    assert isinstance(combinations, ListType)
     all_segments = set(segments)
     for diacritic in diacritics.values():
-        assert isinstance(diacritic, Diacritic)
         for segment in segments:
             new_seg = diacritic.apply(segment)
             if new_seg is not None:
                 all_segments.add(new_seg)
     for combination in combinations:
-        assert isinstance(combination, Combination)
         for segment in segments:
             new_seg = combination.apply(segment)
             if new_seg is not None:
                 all_segments.add(new_seg)
     write_ipa_all(ipa_bases, ipa_all, all_segments, sort_order)
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
