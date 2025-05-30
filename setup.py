418989f9702a739f644208067f71df8d3647d5bd
David R. Mortensen <dmortens@cs.cmu.edu>
2025-05-29 20:05:13 -0400

    Updated configuration

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from setuptools import setup
 
 setup(name='panphon',
-      version='0.21.2',
-      description='Tools for using the International Phonetic Alphabet with phonological features',
+      version='0.22.0',
+      description='Tools for using the International Phonetic Alphabet ' +
+      'with phonological features',
       url='https://github.com/dmort27/panphon',
       download_url='https://github.com/dmort27/panphon/archive/0.21.1.tar.gz',
       long_description=open('README.md', encoding='utf-8').read(),
@@ -22,10 +23,16 @@ setup(name='panphon',
                'panphon/bin/generate_ipa_all.py'],
       packages=['panphon'],
       package_dir={'panphon': 'panphon'},
-      package_data={'panphon': ['data/*.csv', 'data/*.yml']},
+      package_data={
+            'panphon': [
+                  'data/*.csv',
+                  'data/*.yml',
+            ]
+      },
       zip_safe=True,
-      classifiers=['Operating System :: OS Independent',
-                   'Programming Language :: Python :: 3',
-                   'Topic :: Software Development :: Libraries :: Python Modules',
-                   'Topic :: Text Processing :: Linguistic']
+      classifiers=[
+            'Operating System :: OS Independent',
+            'Programming Language :: Python :: 3',
+            'Topic :: Software Development :: Libraries :: Python Modules',
+            'Topic :: Text Processing :: Linguistic']
       )
